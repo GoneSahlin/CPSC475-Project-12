9c955af065ed8a2acdf2da97220a65749035c648
GoneSahlin <zach@sahlins.net>
2022-12-14 16:12:48 -0800

    added read_words

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
 from naive_bayes_classifier import MyNaiveBayesClassifier
 
 
+def read_words(filename):
+    words = []
+    with open(filename, 'r') as infile:
+        lines = infile.readlines()
+        for line in lines:
+            words.append(line.strip())
+
+    return words
+
+
 def main():
+    pos_words_train = read_words('pos.txt')
+    neg_words_train = read_words('neg.txt')
+    
+    print(pos_words_train)
+
+
     clf = MyNaiveBayesClassifier()
 
 
